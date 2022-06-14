17e13df69fd3eb2fa226ac42e6b36e9cdbb09082
Osiris Team <59899645+Osiris-Team@users.noreply.github.com>
2022-06-14 13:19:15 +0200

    removed debug leftovers

--- src/main/java/com/github/alexdlaird/util/NgrokUtils.java
+++ src/main/java/com/github/alexdlaird/util/NgrokUtils.java
@@ -12,9 +12,6 @@ public class NgrokUtils {
         for (int i = 0; i < 10; i++) {
             Thread.sleep(500); // Timeout to ensure the process is shown if it was just created
             for (JProcess p : new ProcessUtils().getThisProcess().childProcesses) {
-                System.out.println(p.name);
-                System.out.println(p.pid);
-                System.out.println(p.command);
                 if(StringUtils.containsIgnoreCase(p.name, "ngrok")){
                     process = p;
                     break;
