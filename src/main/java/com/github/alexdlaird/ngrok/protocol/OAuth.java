0ac368314d2f8dbef44a44d29aa4bf0447a4e6e3
Jan Schweizer <jan@tyx.de>
2023-04-14 09:46:00 +0200

    Apply Google Code Style

--- src/main/java/com/github/alexdlaird/ngrok/protocol/OAuth.java
+++ src/main/java/com/github/alexdlaird/ngrok/protocol/OAuth.java
@@ -5,49 +5,49 @@ import java.util.List;
 import com.google.gson.annotations.SerializedName;
 
 public class OAuth {
-	
-	private String provider;
-	
-	@SerializedName("oauth_scopes")
-    private List<String> scopes;
-	
-	@SerializedName("allow_emails")
-    private List<String> allowEmails;
-	
-	@SerializedName("allow_domains")
-    private List<String> allowDomains;
 
-	public String getProvider() {
-		return provider;
-	}
+  private String provider;
 
-	public void setProvider(String provider) {
-		this.provider = provider;
-	}
+  @SerializedName("oauth_scopes")
+  private List<String> scopes;
 
-	public List<String> getScopes() {
-		return scopes;
-	}
+  @SerializedName("allow_emails")
+  private List<String> allowEmails;
 
-	public void setScopes(List<String> scopes) {
-		this.scopes = scopes;
-	}
+  @SerializedName("allow_domains")
+  private List<String> allowDomains;
 
-	public List<String> getAllowEmails() {
-		return allowEmails;
-	}
+  public String getProvider() {
+    return provider;
+  }
 
-	public void setAllowEmails(List<String> allowEmails) {
-		this.allowEmails = allowEmails;
-	}
+  public void setProvider(String provider) {
+    this.provider = provider;
+  }
 
-	public List<String> getAllowDomains() {
-		return allowDomains;
-	}
+  public List<String> getScopes() {
+    return scopes;
+  }
 
-	public void setAllowDomains(List<String> allowDomains) {
-		this.allowDomains = allowDomains;
-	}
-	
-	
+  public void setScopes(List<String> scopes) {
+    this.scopes = scopes;
+  }
+
+  public List<String> getAllowEmails() {
+    return allowEmails;
+  }
+
+  public void setAllowEmails(List<String> allowEmails) {
+    this.allowEmails = allowEmails;
+  }
+
+  public List<String> getAllowDomains() {
+    return allowDomains;
+  }
+
+  public void setAllowDomains(List<String> allowDomains) {
+    this.allowDomains = allowDomains;
+  }
+
+
 }
