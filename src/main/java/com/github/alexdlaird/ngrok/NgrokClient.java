f684a3bad364291c93c9225b6425dbe5557ade97
Alex Laird <contact@alexlaird.com>
2024-02-26 13:23:03 -0600

    Version bump.

--- src/main/java/com/github/alexdlaird/ngrok/NgrokClient.java
+++ src/main/java/com/github/alexdlaird/ngrok/NgrokClient.java
@@ -183,7 +183,7 @@ public class NgrokClient {
 
     private static final Logger LOGGER = Logger.getLogger(String.valueOf(NgrokClient.class));
 
-    private static final String VERSION = "1.4.9";
+    private static final String VERSION = "1.4.10";
 
     private final JavaNgrokConfig javaNgrokConfig;
     private final NgrokProcess ngrokProcess;
