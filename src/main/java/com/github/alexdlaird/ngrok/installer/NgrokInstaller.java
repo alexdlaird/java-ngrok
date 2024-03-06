3da8e5b3e0befe89c7527e82c27745d6a7283b4f
Alex Laird <contact@alexlaird.com>
2024-03-06 15:18:09 -0600

    Remove unused import.

--- src/main/java/com/github/alexdlaird/ngrok/installer/NgrokInstaller.java
+++ src/main/java/com/github/alexdlaird/ngrok/installer/NgrokInstaller.java
@@ -22,7 +22,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.StringWriter;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
