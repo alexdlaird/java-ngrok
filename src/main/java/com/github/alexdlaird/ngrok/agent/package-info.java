7d040ca1296655a67011cce3ae5f1aea3543ead5
Alex Laird <contact@alexlaird.com>
2025-05-16 09:47:04 -0500

    Add NgrokAgent export so it appears first-class in the docs.

---
+++ src/main/java/com/github/alexdlaird/ngrok/agent/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2021-2025 Alex Laird
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+/**
+ * This package contains functionality related to interacting with the running <code>ngrok</code> agent. Start
+ * at {@link com.github.alexdlaird.ngrok.agent.NgrokAgent}.
+ */
+package com.github.alexdlaird.ngrok.agent;
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -16,6 +16,7 @@ module com.github.alexdlaird.ngrok {
     exports com.github.alexdlaird.exception;
     exports com.github.alexdlaird.http;
     exports com.github.alexdlaird.ngrok;
+    exports com.github.alexdlaird.ngrok.agent;
     exports com.github.alexdlaird.ngrok.conf;
     exports com.github.alexdlaird.ngrok.installer;
     exports com.github.alexdlaird.ngrok.process;
